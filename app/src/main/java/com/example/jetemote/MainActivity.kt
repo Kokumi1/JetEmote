4dbb761f92a7420135f283def806776b29dd159f
Florian <fldebruyck@numericable.fr>
2023-04-15 18:27:35 +0200

    First version push

---
+++ app/src/main/java/com/example/jetemote/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.jetemote
+
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetemote.model.Emote
+import com.example.jetemote.ui.theme.JetEmoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetEmoteTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen(listOf(
+                        Emote(R.drawable.smile,"smile", Color.Yellow),
+                        Emote(R.drawable.neutral,"neutral", Color.Gray),
+                        Emote(R.drawable.sad,"sad", Color.Red)
+                    ))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(emotes: List<Emote>){
+    LazyColumn(
+        //contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    Modifier.fillMaxSize()
+    ){
+        items(
+            items = emotes,
+            itemContent = {
+                EmotesListItem(emote =  it)
+            }
+        )
+    }
+}
+
+@Composable
+fun EmotesListItem(emote: Emote){
+    val context = LocalContext.current
+    Row {
+       Column(modifier = Modifier
+           .clickable { chosen(emote, context) }){
+           Image(painter = painterResource(id = emote.imageId),
+               contentDescription = emote.description,
+               contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = emote.color)
+                    .clip(CircleShape))
+
+       }
+    }
+}
+
+fun chosen(emote : Emote, pContext: Context){
+    Toast.makeText(pContext,"you choose ${emote.description}",Toast.LENGTH_LONG).show()
+}
+
+@Preview
+@Composable
+fun Preview(){
+    MainScreen(listOf(
+        Emote(R.drawable.smile,"smile", Color.Yellow),
+        Emote(R.drawable.neutral,"neutral", Color.Gray),
+        Emote(R.drawable.sad,"sad", Color.Red)
+    ))
+}
---
+++ app/src/main/java/com/example/jetemote/model/Emote.kt
@@ -0,0 +1,8 @@
+package com.example.jetemote.model
+
+import android.media.Image
+import androidx.compose.ui.graphics.Color
+
+class Emote(val imageId: Int,val description: String, val color: Color) {
+
+}
